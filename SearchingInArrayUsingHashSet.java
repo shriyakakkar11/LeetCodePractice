ac270183635e6685d6922e6af87cd87f77b61d54
Shriya <59174545+shriyakakkar11@users.noreply.github.com>
2021-08-19 00:00:00 +0530

    Check If N and Its Double Exist
    iven an array arr of integers, check if there exists two integers N and M such that N is the double of M ( i.e. N = 2 * M).
    
    More formally check if there exists two indices i and j such that :
    
    i != j
    0 <= i, j < arr.length
    arr[i] == 2 * arr[j]

---
+++ SearchingInArrayUsingHashSet.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+class SearchingInArrayUsingHashSet {
+	int[] arr = new int[] {0,0};
+	int count = 0;
+
+	public boolean checkIfExist() {
+		HashSet<Integer> h = new HashSet<Integer>();
+
+		if(arr.length == 0) {
+			return false;
+		}
+
+		for(int i = 0; i < arr.length; i++) {
+			h.add(arr[i]);
+		}
+
+		int twice = 0;
+
+		for(int i = 0; i < arr.length; i++) {
+			if( arr[i] == 0) {
+				count++;
+			}
+			else{
+
+				twice = (2 * arr[i]);
+
+
+				if(h.contains(twice)) {
+
+					return true;
+
+				}
+
+
+				if(((arr[i] % 2) == 0)) {
+
+					int half  = arr[i] / 2;
+
+	
+					if(h.contains(half)) {
+
+						return true;
+
+					}
+
+				}
+
+
+			}
+		}
+
+		if(count > 1) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+		SearchingInArrayUsingHashSet obj1 = new SearchingInArrayUsingHashSet();
+		boolean exists = obj1.checkIfExist();
+		System.out.println("Does there exist 2 indices where one is the double of another? " + exists);
+	}
+}
